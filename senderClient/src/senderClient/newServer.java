f8bc36896f4fa49416f834c46bf727f1850368d7
Connor Goggans <connor.goggans@vanderbilt.edu>
2017-04-30 14:51:27 -0500

    fixed formatting

--- senderClient/src/senderClient/newServer.java
+++ senderClient/src/senderClient/newServer.java
@@ -1,5 +1,4 @@
 package senderClient;
-import java.util.*;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.io.*;
 import java.net.*;
@@ -13,7 +12,7 @@ public class newServer {
 		InputStream audio = null;
 		File file = null;
 		ReentrantReadWriteLock fileLock = new ReentrantReadWriteLock();
-		
+
 		try {
 			ss = new ServerSocket(8888); //create a server socket
 
@@ -22,52 +21,52 @@ public class newServer {
 			Socket socket = ss.accept();
 			//accept and handle the hosting connection
 			//try {
-				//if the socket is connected send the file
-			    //if(socket.isConnected()) {
+			//if the socket is connected send the file
+			//if(socket.isConnected()) {
 
 
 			//audio = new BufferedInputStream(socket.getInputStream());
 			file = new File("audioFile.wav");
 			Thread send = new threadedSender(file, audio, socket, fileLock);
-				    p.execute(send);
-	
-				    System.out.println(file);
-				    //temporary file to store data in
-				    //				    	file = new File("audioFile.wav");
-				    //	byte[] bytes = new byte[2048];
-				    //	audio = new BufferedInputStream(socket.getInputStream());
-				    //	OutputStream out = new FileOutputStream(file);
-				    //	int count;
-				    //	//write out data to the file
-					//		while ((count = audio.read(bytes)) >0) {
-					//	out.write(bytes,0,count);
-				    	//}
-				   	//out.close();
-				    //}
-				    //}catch(IOException e){
-				    //e.printStackTrace();
-				    //}
+			p.execute(send);
+
+			System.out.println(file);
+			//temporary file to store data in
+			//				    	file = new File("audioFile.wav");
+			//	byte[] bytes = new byte[2048];
+			//	audio = new BufferedInputStream(socket.getInputStream());
+			//	OutputStream out = new FileOutputStream(file);
+			//	int count;
+			//	//write out data to the file
+			//		while ((count = audio.read(bytes)) >0) {
+			//	out.write(bytes,0,count);
+			//}
+			//out.close();
+			//}
+			//}catch(IOException e){
+			//e.printStackTrace();
+			//}
 
 			//create thread pool of 10 threads
 			//ExecutorService p = Executors.newFixedThreadPool(10);
 
 			//loop and create worker threads for connections
 			while(true) {
-			    
-			    if(file == null) {
-			    }
-			    else {
-				//System.out.println("Waiting...");
-				Socket s = ss.accept();
-				System.out.println(file);
-				Thread w = new threadedReceiver(s, file, fileLock);
-				//handle the streaming to clients in individual threads
-				System.out.println("Rcv wait 2");
-				p.execute(w);
-			    }
-				
+
+				if(file == null) {
+				}
+				else {
+					//System.out.println("Waiting...");
+					Socket s = ss.accept();
+					System.out.println(file);
+					Thread w = new threadedReceiver(s, file, fileLock);
+					//handle the streaming to clients in individual threads
+					System.out.println("Rcv wait 2");
+					p.execute(w);
+				}
+
 			}
-			
+
 		}catch(IOException e) {
 			e.printStackTrace();
 		}finally {
--- senderClient/src/senderClient/threadedSender.java
+++ senderClient/src/senderClient/threadedSender.java
@@ -7,55 +7,53 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
 
 public class threadedSender extends Thread {
 
-    private static threadedSender instance = null;
-    File file;
-    InputStream audio;
-    Socket socket;
-    ReentrantReadWriteLock fileLock;
-    
-    public void run() {
+	File file;
+	InputStream audio;
+	Socket socket;
+	ReentrantReadWriteLock fileLock;
 
-	try {
-	    boolean connect = socket.isConnected();
-	    System.out.println(connect);
-	    System.out.println("Sending file...");
-	    fileLock.writeLock().lock();
-	    //    file = new File("audioFile.wav");
-	    byte[] bytes = new byte[2048];
-	    audio = new BufferedInputStream(socket.getInputStream());
-	    OutputStream out = new FileOutputStream(file);
-	    int count;
-	    //write out data to the file
-	    while ((count = audio.read(bytes)) >0) {
-		out.write(bytes,0,count);
-	    }
-	    out.close();
-	    fileLock.writeLock().unlock();
-	    System.out.println(file);
-	}catch(IOException e) {
-	    e.printStackTrace();
+	public void run() {
+
+		try {
+			boolean connect = socket.isConnected();
+			System.out.println(connect);
+			System.out.println("Sending file...");
+			fileLock.writeLock().lock();
+			//    file = new File("audioFile.wav");
+			byte[] bytes = new byte[2048];
+			audio = new BufferedInputStream(socket.getInputStream());
+			OutputStream out = new FileOutputStream(file);
+			int count;
+			//write out data to the file
+			while ((count = audio.read(bytes)) >0) {
+				out.write(bytes,0,count);
+			}
+			out.close();
+			fileLock.writeLock().unlock();
+			System.out.println(file);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
-    }
 
-    public threadedSender(File f, InputStream a, Socket s, ReentrantReadWriteLock l) {
-	this.file = f;
-	this.audio = a;
-	this.socket = s;
-	this.fileLock = l;
-    }
+	public threadedSender(File f, InputStream a, Socket s, ReentrantReadWriteLock l) {
+		this.file = f;
+		this.audio = a;
+		this.socket = s;
+		this.fileLock = l;
+	}
 
-    //public static threadedSender getSender(File f, InputStream a) {
-    //	if (instance == null) {
-    //	    instance = new threadedSender();
-    //	    instance.file = f;
-    //	    instance.audio = a;
-	    //instance.socket = s;
-    //	}
-    //	return instance;
-    //}
+	//public static threadedSender getSender(File f, InputStream a) {
+	//	if (instance == null) {
+	//	    instance = new threadedSender();
+	//	    instance.file = f;
+	//	    instance.audio = a;
+	//instance.socket = s;
+	//	}
+	//	return instance;
+	//}
 
 };
